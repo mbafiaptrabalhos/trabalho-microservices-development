54e24c4a2377b9b03162689fc4285c7501246da9
Jonathas Garcia <jonathas-garcia>
2020-12-15 19:34:03 -0300

    Ajuste pastas

--- frontend-controle-financeiro/karma.conf.js
+++ frontend-controle-financeiro/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/controle-financeiro'),
+      dir: require('path').join(__dirname, './coverage/frontend-controle-financeiro'),
       subdir: '.',
       reporters: [
         { type: 'html' },
